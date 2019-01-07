5d9ae5e1bf6eea62ce0f263048a5ac6948cd3f16
sunzhen <1098174033@qq.com>
2019-01-07 16:43:06 +0800

    更新iop-web.sql

--- src/main/iop-web.sql
+++ src/main/iop-web.sql
@@ -1,8 +1,8 @@
 /*
-SQLyog 企业版 - MySQL GUI v8.14
+SQLyog 企业版 - MySQL GUI v8.14 
 MySQL - 5.7.21 : Database - iop-web
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -39,13 +39,17 @@ DROP TABLE IF EXISTS `iop_sys_menu`;
 CREATE TABLE `iop_sys_menu` (
   `menu_id` int(11) NOT NULL,
   `menu_name` varchar(64) NOT NULL,
+  `menu_url` varchar(2048) DEFAULT '#',
   `menu_level` int(11) DEFAULT NULL,
   `parent_mennu_id` varchar(1024) DEFAULT NULL,
   `menu_order` int(11) DEFAULT NULL,
+  `authc_Id` int(11) NOT NULL,
   PRIMARY KEY (`menu_id`,`menu_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `iop_sys_menu` */
+
+insert  into `iop_sys_menu`(`menu_id`,`menu_name`,`menu_url`,`menu_level`,`parent_mennu_id`,`menu_order`,`authc_Id`) values (1001,'系统管理','#',1,'0',1,0),(2001,'用户管理','/user/page',2,'1001',1,1001),(2002,'角色管理','/role/page',2,'1001',2,1002),(2003,'权限管理','/authc/page',2,'1001',3,1003),(2004,'菜单管理','/menu/page',2,'1001',4,1004);
 
 /*Table structure for table `iop_sys_role` */
 
@@ -72,11 +76,11 @@ CREATE TABLE `iop_sys_role_authc` (
   `role_id` int(11) DEFAULT NULL,
   `authc_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`role_authc_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=107 DEFAULT CHARSET=utf8;
 
 /*Data for the table `iop_sys_role_authc` */
 
-insert  into `iop_sys_role_authc`(`role_authc_id`,`role_id`,`authc_id`) values (31,2,1001),(32,2,1001001),(33,2,1001002),(34,2,1001003),(35,2,1001004),(36,2,1001005),(37,2,1001006),(38,2,1001007),(39,2,1002),(40,2,1002001),(41,2,1002002),(42,2,1002003),(43,2,1002004),(44,2,1002005),(45,2,1002006),(46,2,1002007),(47,2,1003),(48,2,1003001),(49,2,1003002),(50,2,1003003),(51,2,1003004),(52,2,1003005),(63,1,1001),(64,1,1001001),(65,1,1001002),(66,1,1001003),(67,1,1001004),(68,1,1001005),(69,1,1001006),(70,1,1001007),(71,1,1002),(72,1,1002001),(73,1,1002002),(74,1,1002003),(75,1,1002004),(76,1,1002005),(77,1,1002006),(78,1,1002007),(79,1,1003),(80,1,1003001),(81,1,1003002),(82,1,1003003),(83,1,1003004),(84,1,1003005),(86,8,1003),(87,8,1003001),(88,8,1003003);
+insert  into `iop_sys_role_authc`(`role_authc_id`,`role_id`,`authc_id`) values (32,2,1001001),(33,2,1001002),(34,2,1001003),(35,2,1001004),(36,2,1001005),(37,2,1001006),(38,2,1001007),(39,2,1002),(40,2,1002001),(41,2,1002002),(42,2,1002003),(43,2,1002004),(44,2,1002005),(45,2,1002006),(46,2,1002007),(47,2,1003),(48,2,1003001),(49,2,1003002),(50,2,1003003),(51,2,1003004),(52,2,1003005),(64,1,1001001),(65,1,1001002),(66,1,1001003),(67,1,1001004),(68,1,1001005),(69,1,1001006),(70,1,1001007),(71,1,1002),(72,1,1002001),(73,1,1002002),(74,1,1002003),(75,1,1002004),(76,1,1002005),(77,1,1002006),(78,1,1002007),(79,1,1003),(80,1,1003001),(81,1,1003002),(82,1,1003003),(83,1,1003004),(84,1,1003005),(92,1,1004),(93,1,1005),(95,6,1001001),(96,6,1001006),(97,6,1002),(98,6,1002001),(100,6,1002006),(101,6,1003),(102,6,1003001),(104,1,1001),(105,6,1001),(106,2,1001);
 
 /*Table structure for table `iop_sys_user` */
 
@@ -94,7 +98,7 @@ CREATE TABLE `iop_sys_user` (
 
 /*Data for the table `iop_sys_user` */
 
-insert  into `iop_sys_user`(`user_id`,`username`,`nickname`,`password`) values (1,'admin','超级管理员','123456'),(2,'authcAdmin','权限管理员','123456'),(39,'sunzhen','开发人员','123456'),(40,'bravo','测试人员','123456');
+insert  into `iop_sys_user`(`user_id`,`username`,`nickname`,`password`) values (1,'admin','超级管理员','21232f297a57a5a743894a0e4a801fc3'),(2,'authcAdmin','权限管理员','e10adc3949ba59abbe56e057f20f883e'),(39,'sunzhen','开发人员','e10adc3949ba59abbe56e057f20f883e'),(40,'bravo','测试人员','e10adc3949ba59abbe56e057f20f883e');
 
 /*Table structure for table `iop_sys_user_role` */
 
